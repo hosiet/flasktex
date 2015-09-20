280f2f32be9a12d50692f4d586c212ae6906ebd4
Boyuan Yang <073plan@gmail.com>
2015-09-20 20:46:35 +0800

    testing for route

--- flasktex/views.py
+++ flasktex/views.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 from flasktex import app
+from flask import make_response, render_template
 
 @app.route("/")
-def ft_welcome():
+def ft_webpage_welcome():
     return 'Hello world!'
+
+@app.route("/about")
+def ft_webpage_about():
+    resp = make_response;
+    resp.headers['Content-Type'] = 'application/xhtml+xml'
+    resp.data = render_template('common.html', title="关于", body="示例")
+    return resp
